fc4c0ac326ba92590b8003ee9545bcf359b599fe
trondeh80 <trond.erling@gmail.com>
2020-10-31 10:05:47 +0100

    Add tailwindcss

--- migrate-fe/src/App.js
+++ migrate-fe/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>XP Data Migration Tool</h1>
       </header>
+      <div className="alert wide-alert hidden">
+
+      </div>
+      <main>
+      <div className="flex flex-row bg-gray-200">
+        <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
+        <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
+        <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">3</div>
+      </div>
+      </main>
+      <footer>
+
+      </footer>
     </div>
   );
 }
