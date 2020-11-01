21859a3a3a9bd6ef242aa8b0be62565da4044447
trondeh80 <trond.erling@gmail.com>
2020-11-01 13:15:42 +0100

    Fix enums. Add FE build files.

--- datamigrate/build/resources/main/assets/js/data-migrate.js
+++ datamigrate/build/resources/main/assets/js/data-migrate.js
@@ -1,13 +1,3 @@
 (async () => {
   // init --//--
-  try {
-    const editorDom = document.getElementById('editor');
-    const editor = CodeMirror(editorDom, {
-      value: "{ count: 10, start: 0 }\n",
-      mode:  "javascript"
-    });
-    console.log('Editor is ready to work!');
-  } catch (reason) {
-    console.log(`Editor initialization failed; ${reason}`);
-  }
 })();
